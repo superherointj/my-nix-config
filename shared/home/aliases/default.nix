61cded41fd629254abb8071750bd1799fefe5dda
xsamxz <samuelwww2000@gmail.com>
2023-03-08 09:02:20 -0300

    create aliases

---
+++ shared/home/aliases/default.nix
@@ -0,0 +1,14 @@
+{pkgs}:
+
+{
+    ga = "git add";
+    gs = "git status";
+    gp = "git push origin -u main";
+    gc = "git commit -m";
+
+    s = "sudo su";
+
+    v = "vim";
+
+    cdhm = "cd /etc/nixos/shared/home";
+}
