26b57bdd2649c3a26feed8d4652233955eff1985
xsamxz <samuelwww2000@gmail.com>
2023-03-16 08:32:20 -0300

    fix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
 
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.elitebook = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         (./. + "/hosts/elitebook/default.nix")
